5cb76d93ec61c859033f28c67a1c75143398fb1c
Oleksandr Lozinskyy <alexander.lozinsky.1977@gmail.com>
2021-07-21 01:23:51 +0300

    Initial commit of Lesson2 Home Work

---
+++ Lesson_2/task_import.py
@@ -0,0 +1,11 @@
+#  import from __future__  print's python developers joke "SyntaxError: not a chance"
+# from __future__ import braces
+
+# import this print Zen of Python
+import this
+
+# import antigravity open site https://xkcd.com/353/
+import antigravity
+
+# This import print "Hello world!"
+import __hello__
---
+++ README.md
@@ -0,0 +1,1 @@
+Hillel School Advanced Python Course

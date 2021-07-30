eb7dd97f261f742c581222c1d72ac23e566f011d
Oleksandr Lozinskyy <alexander.lozinsky.1977@gmail.com>
2021-07-31 00:18:43 +0300

    Домашнее задание к уроку 4. Апофеоз оптимизации в reversed_phrase.py

--- Lesson4/reversed_phrase.py
+++ Lesson4/reversed_phrase.py
@@ -2,7 +2,6 @@ phrase = ''
 while True:
     phrase = input('Введите два слова. Чтобы прекратить введите q :')
     if phrase != 'q':
-        phrase1 = phrase.lower()
-        print(phrase, '=>', phrase1[::-1].title())
+        print(phrase, '=>', phrase.lower()[::-1].title())
     else:
         break

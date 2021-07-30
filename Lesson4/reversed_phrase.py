cecc73a23ba812313d9a4f62a2e11f8868ddd716
Oleksandr Lozinskyy <alexander.lozinsky.1977@gmail.com>
2021-07-30 23:38:56 +0300

    Домашнее задание к уроку 4. Сокращена запись кода в файле reversed_phrase.py

--- Lesson4/reversed_phrase.py
+++ Lesson4/reversed_phrase.py
@@ -2,8 +2,7 @@ phrase = ''
 while True:
     phrase = input('Введите два слова. Чтобы прекратить введите q :')
     if phrase != 'q':
-        lst = phrase.lower().split()
-        lst = lst[::-1]
+        lst = (phrase.lower().split())[::-1]
         phrase1 = ' '.join(lst[::-1])
         print(phrase, '=>', phrase1[::-1].title())
     else:

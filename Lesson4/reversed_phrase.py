4ae4826e687762ef93a3ee7f251a3a7fdec3530a
Oleksandr Lozinskyy <alexander.lozinsky.1977@gmail.com>
2021-07-31 00:16:13 +0300

    Домашнее задание к уроку 4. Оптимизирован код в reversed_phrase.py Убрал лишние итерации

--- Lesson4/reversed_phrase.py
+++ Lesson4/reversed_phrase.py
@@ -2,8 +2,7 @@ phrase = ''
 while True:
     phrase = input('Введите два слова. Чтобы прекратить введите q :')
     if phrase != 'q':
-        lst = (phrase.lower().split())[::-1]
-        phrase1 = ' '.join(lst[::-1])
+        phrase1 = phrase.lower()
         print(phrase, '=>', phrase1[::-1].title())
     else:
         break

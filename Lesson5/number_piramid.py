b998cbdf47c1ba775f343741df712adf6c17da4d
Oleksandr Lozinskyy <alexander.lozinsky.1977@gmail.com>
2021-08-08 11:39:54 +0300

    Домашнее задание к уроку 5. Добавлена проверка на отрицательные и дробные числа в обеих задачах

--- Lesson5/number_piramid.py
+++ Lesson5/number_piramid.py
@@ -1,7 +1,17 @@
 entered_num = input('Input number from 3 to 9: ')
 passed_number = entered_num.isdigit()  # Проверяем что введенные символы цифры
 
-if passed_number:
+if not passed_number:
+    if entered_num[0] == '-' and entered_num[1:].isdigit():
+        print(f'Введенное значение должно быть положительным числом. Работа приложения завершена')
+    elif entered_num.find('.') >= 0 and (entered_num.replace('.', '', 1)).isdigit():
+        print(f'Введенное значение не должно быть дробным. Попробуйте еще раз!')
+    elif entered_num[0] == '-' and entered_num.find('.') >= 1 and (entered_num[1:].replace('.', '', 1)).isdigit():
+        print(f'Введенное значение не должно быть отрицательным и дробным. Попробуйте еще раз!')
+    else:
+        # В случае, если введен текст или символы, выводим сообщение об ошибке и завершаем работу
+        print('Введено не число! Работа приложения завершена')
+elif passed_number:
     entered_num = int(entered_num)  # Так как ранее мы проверили, что введенные значения цифры, то переводим его в число
     if entered_num in range(3, 10):
         for i in range(1, entered_num + 1):
@@ -17,6 +27,3 @@ if passed_number:
     else:
         print('Введено число за пределами диапазона! Работа приложения завершена')
 
-else:
-    # В случае, если введено не число, выводим сообщение об ошибке и завершаем работу
-    print('Введено не число! Работа приложения завершена')

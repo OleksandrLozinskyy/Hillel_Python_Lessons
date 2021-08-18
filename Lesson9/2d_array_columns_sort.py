8f611ac3e69c5fbdec093000a81d0ea0db83ec6f
Oleksandr Lozinskyy <alexander.lozinsky.1977@gmail.com>
2021-08-18 10:43:43 +0300

    Домашнее задание к уроку 9. Алгоритмы

--- Lesson9/2d_array_columns_sort.py
+++ Lesson9/2d_array_columns_sort.py
@@ -27,10 +27,19 @@ for n in range(len(gen_arr)):
         column_sum += gen_arr[n][m]
     sum_lst.append(column_sum)
 
+# В случае необходимости можно раскомментировать и напечатать исходный массив
 # print(gen_arr, '\n')
 
 
-def print_array(arr, sum_lst):
+def print_array(arr, lst):
+    """
+    Функция для красивой печати массива
+    Печатаем элементы построчно, между элементами вставляем пробелы.
+    Для того чтоб отступы были одинаковые, то разбиваем по количеству символов в элементе массива
+    Для двух символьных добавляем по два пробела спереди, для односимвольных по три.
+    По окончанию печати массива печатаем строку с суммами каждого столбца, также добавляем пробелы
+    для соблюдения отступов
+    """
     for j in range(column_count):
         for i in range(column_count):
             if len(str(gen_arr[i][j])) == 1 and j == 0:
@@ -42,15 +51,22 @@ def print_array(arr, sum_lst):
             elif len(str(gen_arr[i][j])) == 2 and j > 0:
                 print('  ', gen_arr[i][j], end=' ')
         print('\n')
-    for n in range(len(sum_lst)):
+    for n in range(len(lst)):
         if n == 0:
-            print(' ',sum_lst[n], end='  ')
+            print(' ', lst[n], end='  ')
+        elif len(str(lst[n])) == 2:
+            print(' ', lst[n], end='  ')
         else:
-            print('', sum_lst[n], end='  ')
+            print('', lst[n], end='  ')
     print('\n')
 
 
 def sort_array_column(arr):
+    """
+    Функция сортировки значений в столбцах массива.
+    Так как по условию задания в нечетных столбцах сортировка должна быть по возрастанию, а в четных по убыванию,
+    то проверяем индекс столбца и разделяем код для сортировки.
+    """
     for item in range(column_count):
         lst = arr[item]
 
@@ -66,9 +82,10 @@ def sort_array_column(arr):
                         lst[j], lst[j + 1] = lst[j + 1], lst[j]
 
 
+# Собственно печатаем результаты. Сначала несортированный массив, затем сортируем его
+# и печатаем сортированный массив
 print('\n\n')
 print_array(gen_arr, sum_lst)
 print('\n\n')
 sort_array_column(gen_arr)
-gen_arr.append(sum_lst)
 print_array(gen_arr, sum_lst)

a92ebeb05a7ce03ef812b66d039da2299660cd1b
Oleksandr Lozinskyy <alexander.lozinsky.1977@gmail.com>
2021-08-25 19:42:49 +0300

    Домашнее задание к уроку 9. Алгоритмы. Переделано ДЗ согласно задания

--- Lesson9/2d_array_columns_sort.py
+++ Lesson9/2d_array_columns_sort.py
@@ -6,7 +6,7 @@ while True:
     column_count = input("Введите размер массива для генерации (column_count должен быть больше 5): ")
     if str(column_count) == 'q':
         sys.exit()
-    elif not str(column_count).isdigit() or int(column_count) <= 5:
+    elif not str(column_count).isdigit() or int(column_count) < 5:
         print('Введенное значение не удовлетворяет требованию. Значение должно быть целым числом больше 5.\n \
         Попробуйте еще раз или нажмите q для выхода.')
         continue
@@ -16,23 +16,15 @@ while True:
 print(f'Исходный массив будет иметь размер М*М - {column_count}*{column_count}')
 
 column_count = int(column_count)
-# Создаем пустой список для сумм столбцов
-sum_lst = []
+
 # Создаем исходный массив М*М
 gen_arr = [[randrange(1, 50) for i in range(column_count)] for _ in range(column_count)]
 
-# Считаем сумму столбца.
-for n in range(len(gen_arr)):
-    column_sum = 0
-    for m in range(len(gen_arr[n])):
-        column_sum += gen_arr[n][m]
-    sum_lst.append(column_sum)
-
 # В случае необходимости можно раскомментировать и напечатать исходный массив
-# print(gen_arr, '\n')
+#print(gen_arr, '\n')
 
 
-def print_array(arr, lst):
+def print_array(arr):
     """
     Функция для красивой печати массива
     Печатаем элементы построчно, между элементами вставляем пробелы.
@@ -41,52 +33,65 @@ def print_array(arr, lst):
     По окончанию печати массива печатаем строку с суммами каждого столбца, также добавляем пробелы
     для соблюдения отступов
     """
-    for j in range(column_count):
-        for i in range(column_count):
-            if len(str(arr[i][j])) == 1 and j == 0:
-                print('   ', arr[i][j], end=' ')
-            elif len(str(arr[i][j])) == 2 and j == 0:
-                print('  ', arr[i][j], end=' ')
-            if len(str(arr[i][j])) == 1 and j > 0:
-                print('   ', arr[i][j], end=' ')
-            elif len(str(arr[i][j])) == 2 and j > 0:
-                print('  ', arr[i][j], end=' ')
-        print('\n')
-    for n in range(len(lst)):
-        if n == 0:
-            print(' ', lst[n], end='  ')
-        elif len(str(lst[n])) == 2:
-            print(' ', lst[n], end='  ')
-        else:
-            print('', lst[n], end='  ')
-    print('\n')
+    print()
+    for i in range(column_count + 1):
+        for j in range(column_count):
+            print(f"{arr[i][j] : > 4}", end="")
+        print()
 
-
-def sort_array_column(arr):
+def sum_and_sort_array_column(arr):
     """
     Функция сортировки значений в столбцах массива.
     Так как по условию задания в нечетных столбцах сортировка должна быть по возрастанию, а в четных по убыванию,
     то проверяем индекс столбца и разделяем код для сортировки.
     """
-    for item in range(column_count):
-        lst = arr[item]
 
-        if item % 2 == 0:
-            for i in range(column_count - 1):
-                for j in range(column_count - i - 1):
-                    if lst[j] < lst[j + 1]:
-                        lst[j], lst[j + 1] = lst[j + 1], lst[j]
-        elif item % 2 > 0:
-            for i in range(column_count - 1):
-                for j in range(column_count - i - 1):
-                    if lst[j] > lst[j + 1]:
-                        lst[j], lst[j + 1] = lst[j + 1], lst[j]
+    # Подсчет сумм столбцов и добавление их элементом массива
+
+    sum_list = []
+
+    for j in range(column_count):
+        column_sum = 0
+        for i in range(column_count):
+            column_sum += arr[i][j]
+        sum_list.append(column_sum)
+    arr.append(sum_list)
+
+    # Печать несортированного массива с суммами столбцов
+    print('\n')
+    print('Несортированный массив с суммами столбцов', end='\n')
+    print_array(arr)
+
+    # Сортируем положение столбцов по возрастанию суммы
+
+    for n in range(column_count - 1):
+        for j in range(column_count - n - 1):
+            if arr[column_count][j] > arr[column_count][j + 1]:
+                arr[column_count][j], arr[column_count][j + 1] = \
+                    arr[column_count][j + 1], arr[column_count][j]
+                for i in range(column_count):
+                    arr[i][j], arr[i][j + 1] = arr[i][j + 1], arr[i][j]
+
+    # Сортируем элементы в столбцах. В четных по убыванию, в нечетных по возрастанию
+
+    for j in range(column_count):
+        if j % 2 == 0:
+            for n in range(column_count - 1):
+                for i in range(column_count - n - 1):
+                    if arr[i][j] < arr[i + 1][j]:
+                        arr[i][j], arr[i + 1][j] = arr[i + 1][j], arr[i][j]
+        elif j % 2 > 0:
+            for n in range(column_count - 1):
+                for i in range(column_count - n - 1):
+                    if arr[i][j] > arr[i + 1][j]:
+                        arr[i][j], arr[i + 1][j] = arr[i + 1][j], arr[i][j]
+    # Печатаем результирующий отсортированный массив
+    print('\n')
+    print('Сортированный массив с суммами столбцов', end='\n')
+    print_array(arr)
 
 
-# Собственно печатаем результаты. Сначала несортированный массив, затем сортируем его
-# и печатаем сортированный массив
-print('\n\n')
-print_array(gen_arr, sum_lst)
-print('\n\n')
-sort_array_column(gen_arr)
-print_array(gen_arr, sum_lst)
+# Вызываем функцию сортировки и передаем ей как параметр сгенерированный массив
+
+sum_and_sort_array_column(gen_arr)
+

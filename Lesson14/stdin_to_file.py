4f76e2ffcf9e51003e3cfb9fc3258696c243d24d
Oleksandr Lozinskyy <alexander.lozinsky.1977@gmail.com>
2021-09-07 08:48:10 +0300

    Домашнее задание к уроку 14. Файлы, работа с файлами

---
+++ Lesson14/stdin_to_file.py
@@ -0,0 +1,18 @@
+import string
+import random
+
+filename = ''.join(random.choice(string.ascii_uppercase + string.digits
+                   + string.ascii_lowercase) for _ in range(8)) + '.txt'
+
+with open(filename, 'w', encoding='utf-8') as out_file:
+    while True:
+        user_input = input('Введите текст для записи в файл, для завершения \
+        введите пустую строку: ')
+        if user_input != '':
+            line = user_input + '\n'
+            out_file.write(line)
+        else:
+            out_file.close()
+            break
+
+
